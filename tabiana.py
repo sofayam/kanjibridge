351cc61c91ee3f36270e077c728c1acd7596e963
Mark Andrew <mark.andrew@gmail.com>
2012-12-21 11:02:42 +0100

    tabiana_noodling

--- tabiana.py
+++ tabiana.py
@@ -1,9 +1,14 @@
+import jchars
 import codecs
 import mergeinput
 
 dict = {}
 
+pops = {}
+
 def adddict(u):
+    if not jchars.isKanji(u):
+        return 
     if u not in dict.keys():
         dict[u] = 0
     dict[u] += 1
@@ -21,17 +26,37 @@ for u in txt:
 m = mergeinput.merge()
 
 hdict = {}
-
+normalctr = 0
+obscurectr = 0 
+vobscurectr = 0 
 for k in m.keys():
     hdict[m[k]['glyph'].decode('utf-8')] = k
 
 for k in sorted(dict.keys()):
     obscure = "*** obscure"
+ 
     if k not in hdict.keys():
         res = "*** very obscure"
+        vobscurectr += 1 
     else:
         res = m[hdict[k]]['keyword']
-        if hdict[k] < 2020: 
+        if hdict[k] <= 2042:
+            normalctr += 1 
             obscure = ""
+        else:
+            obscurectr += 1
     print dict[k],k,res,obscure
+
+for k in dict.keys():
+    count = dict[k]
+    if count not in pops.keys():
+        pops[count] = []
+    pops[count].append(k)
+
+print "%d normal %d obscure %d very obscure" % (normalctr, obscurectr, vobscurectr)
+
+for pop in pops.keys():
+    print pop
+    for k in pops[pop]: print k,
+    print
     
